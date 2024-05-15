dff598339b5c30f641bad0606eb6850194bd2a07
Uttam Kumar <uttammaurya377@gmail.com>
2024-05-15 22:43:18 +0530

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import Product from './Product'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>Main component</div>
+     <Product  name="Samsung" price="70000" />
+     <Product name="Iphone 14 " price="90000"/> 
+     
+    </>
+  )
+}
+
+export default App
---
+++ my-react-app/src/Product.jsx
@@ -0,0 +1,13 @@
+
+
+function Product(props){
+
+    return(
+        <div className="product">
+            <h3>{props.name}</h3>
+            <h3>{props.price}</h3>
+           
+        </div>
+    )
+}
+export default Product;
